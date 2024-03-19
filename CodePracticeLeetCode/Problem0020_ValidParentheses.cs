15fce7ab12b0b9efa25856628da15ea104ba2ef1
Антон <Strateg7602@gmail.com>
2024-03-19 09:53:17 +0300

    LeetCode Problem 20 - ValidParentheses Solved

---
+++ CodePracticeLeetCode/Problem0020_ValidParentheses.cs
@@ -0,0 +1,65 @@
+﻿namespace CodePracticeLeetCode
+{
+    /*
+     * Given a string s containing just the characters '(', ')', '{', '}', '[' and ']', determine if the input string is valid.
+     * An input string is valid if:
+     * - Open brackets must be closed by the same type of brackets.
+     * - Open brackets must be closed in the correct order.
+     * - Every close bracket has a corresponding open bracket of the same type.
+     */
+    public class Problem0020_ValidParentheses : IProblem
+    {
+        private string? _s { get; set; }
+        private bool _output { get; set; }
+        public bool Solve(string? s)
+        {
+            Stack<char> brakets = new Stack<char>();
+            foreach (char symbol in s)
+            {
+                switch (symbol)
+                {
+                    case '(': { brakets.Push(symbol); break; }
+                    case '[': { brakets.Push(symbol); break; }
+                    case '{': { brakets.Push(symbol); break; }
+                    case ')':
+                        {
+                            if (brakets.TryPeek(out char lastBracket) && lastBracket.Equals('(')) brakets.Pop();
+                            else return false;
+                            break;
+                        }
+                    case '}':
+                        {
+                            if (brakets.TryPeek(out char lastBracket) && lastBracket.Equals('{')) brakets.Pop();
+                            else return false;
+                            break;
+                        }
+                    case ']':
+                        {
+                            if (brakets.TryPeek(out char lastBracket) && lastBracket.Equals('[')) brakets.Pop();
+                            else return false;
+                            break;
+                        }
+                }
+            }
+            return !brakets.TryPeek(out char anyBracket);
+        }
+
+
+        public void ReadInput()
+        {
+            Console.WriteLine("Enter a string (s):");
+            _s = Console.ReadLine();
+        }
+
+        public void ShowAnswer()
+        {
+            Console.WriteLine("Answer:");
+            Console.WriteLine(_output);
+        }
+
+        public void Solve()
+        {
+            _output = Solve(_s);
+        }
+    }
+}
---
+++ CodePracticeLeetCodeTests/Problem0020_ValidParentheses_Tests.cs
@@ -0,0 +1,44 @@
+﻿namespace CodePracticeLeetCodeTests
+{
+    internal class Problem0020_ValidParentheses_Tests
+    {
+        private Problem0020_ValidParentheses problem;
+        [SetUp]
+        public void Setup()
+        {
+            problem = new Problem0020_ValidParentheses();
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            string s = "()";
+            bool output = true;
+            Assert.That(problem.Solve(s), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            string s = "()[]{}";
+            bool output = true;
+            Assert.That(problem.Solve(s), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            string s = "(]";
+            bool output = false;
+            Assert.That(problem.Solve(s), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            string s = "(";
+            bool output = false;
+            Assert.That(problem.Solve(s), Is.EqualTo(output));
+        }
+    }
+}
