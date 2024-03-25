33cd8370fbe36fb8e779108e30f587939226e953
Антон <Strateg7602@gmail.com>
2024-03-25 10:09:51 +0300

    LeetCode Problem 26 - RemoveDuplicatesFromSortedArray Solved

---
+++ CodePracticeLeetCode/Problem0026_RemoveDuplicatesFromSortedArray.cs
@@ -0,0 +1,40 @@
+﻿namespace CodePracticeLeetCode
+{
+    public class Problem0026_RemoveDuplicatesFromSortedArray : IProblem
+    {
+        private int[] _nums { get; set; } = new int[0];
+        private int _output { get; set; }
+
+        public int RemoveDuplicates(int[] nums)
+        {
+            int indexSlow = 1;
+            for (int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] != nums[i - 1])
+                {
+                    if (indexSlow != i)
+                    {
+                        nums[indexSlow] = nums[i];
+                    }
+                    indexSlow++;
+                }
+            }
+            return indexSlow;
+        }
+
+        public void ReadInput()
+        {
+            _nums = ProblemIO.ReadIntArrayFromConsole("Input original array (nums):");
+        }
+
+        public void ShowAnswer()
+        {
+            ProblemIO.WriteValue(_output);
+        }
+
+        public void Solve()
+        {
+            _output = RemoveDuplicates(_nums);
+        }
+    }
+}
---
+++ CodePracticeLeetCodeTests/Problem0026_RemoveDuplicatesFromSortedArray_Tests.cs
@@ -0,0 +1,40 @@
+﻿using System.Globalization;
+
+namespace CodePracticeLeetCodeTests
+{
+    internal class Problem0026_RemoveDuplicatesFromSortedArray_Tests
+    {
+        private Problem0026_RemoveDuplicatesFromSortedArray problem;
+        [SetUp]
+        public void Setup()
+        {
+            problem = new Problem0026_RemoveDuplicatesFromSortedArray();
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            int[] nums = { 1, 1, 2 };
+            int[] expectedNums = { 1, 2, 2 };
+            int output = 2;
+            Assert.That(problem.RemoveDuplicates(nums), Is.EqualTo(output));
+            for (int i = 0; i < output; i++)
+            {
+                Assert.That(nums[i], Is.EqualTo(expectedNums[i]));
+            }
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            int[] nums = { 0, 0, 1, 1, 1, 2, 2, 3, 3, 4 };
+            int[] expectedNums = { 0, 1, 2, 3, 4, 2 };
+            int output = 5;
+            Assert.That(problem.RemoveDuplicates(nums), Is.EqualTo(output));
+            for (int i = 0; i < output; i++)
+            {
+                Assert.That(nums[i], Is.EqualTo(expectedNums[i]));
+            }
+        }
+    }
+}
