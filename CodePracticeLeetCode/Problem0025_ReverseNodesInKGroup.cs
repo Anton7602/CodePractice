6696fb3a69efaa4e3497cd6779b9e34a8cbf8c14
Антон <Strateg7602@gmail.com>
2024-03-27 08:55:22 +0300

    LeetCode Problem 25 - ReverseNodesInKGroup Solved

---
+++ CodePracticeLeetCode/Problem0025_ReverseNodesInKGroup.cs
@@ -0,0 +1,47 @@
+﻿namespace CodePracticeLeetCode
+{
+    public class Problem0025_ReverseNodesInKGroup : IProblem
+    {
+        /*
+         * Given the head of a linked list, reverse the nodes of the list k at a time, and return the modified list.
+         * k is a positive integer and is less than or equal to the length of the linked list. If the number of nodes is not a multiple of k then left-out nodes, in the end, should remain as it is.
+         * You may not alter the values in the list's nodes, only nodes themselves may be changed.
+         */
+        private ListNode _head {  get; set; }
+        private int _k {  get; set; }
+        private ListNode _output { get; set; }
+
+        public ListNode ReverseKGroup(ListNode head, int k)
+        {
+            Stack<ListNode> nodeStack = new Stack<ListNode>();
+            ListNode tempNode = head;
+            for (int i=0; i<k; i++)
+            {
+                if (tempNode == null) return head;
+                nodeStack.Push(tempNode);
+                tempNode = tempNode.next;
+            }
+            tempNode = nodeStack.Peek();
+            head.next = ReverseKGroup(tempNode.next, k);
+            while(nodeStack.Count>1)
+                nodeStack.Pop().next = nodeStack.Peek();
+            return tempNode;
+        }
+
+        public void ReadInput()
+        {
+            _head = ProblemIO.ReadListNodeFromConsole("Input a linked list (head):");
+            _k = ProblemIO.ReadIntFromConsole("Input length of reverse node (k):");
+        }
+
+        public void ShowAnswer()
+        {
+            ProblemIO.WriteValue(_output);
+        }
+
+        public void Solve()
+        {
+            _output = ReverseKGroup(_head, _k);
+        }
+    }
+}
---
+++ CodePracticeLeetCodeTests/Problem0025_ReverseNodesInKGroup_Tests.cs
@@ -0,0 +1,30 @@
+﻿namespace CodePracticeLeetCodeTests
+{
+    internal class Problem0025_ReverseNodesInKGroup_Tests
+    {
+        private Problem0025_ReverseNodesInKGroup problem;
+        [SetUp]
+        public void Setup()
+        {
+            problem = new Problem0025_ReverseNodesInKGroup();
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+            int k = 2;
+            ListNode output = new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(3, new ListNode(5)))));
+            Assert.That(problem.ReverseKGroup(head, k), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            ListNode head = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))));
+            int k = 3;
+            ListNode output = new ListNode(3, new ListNode(2, new ListNode(1, new ListNode(4, new ListNode(5)))));
+            Assert.That(problem.ReverseKGroup(head, k), Is.EqualTo(output));
+        }
+    }
+}
