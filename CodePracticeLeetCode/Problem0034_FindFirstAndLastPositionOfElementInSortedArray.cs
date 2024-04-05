3d19718db42402a44cfd8462aa3540eb7f5e0cd7
Антон <Strateg7602@gmail.com>
2024-04-05 09:00:02 +0300

    LeetCode Problem 34 - FindFirstAndLastPositionOfElementInSortedArray Solved

---
+++ CodePracticeLeetCode/Problem0034_FindFirstAndLastPositionOfElementInSortedArray.cs
@@ -0,0 +1,98 @@
+﻿namespace CodePracticeLeetCode
+{
+    /*
+     * Given an array of integers nums sorted in non-decreasing order, find the starting and ending position of a given target value.
+     * If target is not found in the array, return [-1, -1].
+     * You must write an algorithm with O(log n) runtime complexity.
+     */
+    public class Problem0034_FindFirstAndLastPositionOfElementInSortedArray : IProblem
+    {
+        private int[] _nums { get; set; } = new int[0];
+        private int _target { get; set; }
+        private int[] _output { get; set; } = new int[0];
+
+        public int[] SearchRange(int[] nums, int target)
+        {
+            int leftIndexer = 0;
+            int rightIndexer = nums.Length - 1;
+            int midIndexer = (leftIndexer + rightIndexer) / 2;
+            while (leftIndexer <= rightIndexer)
+            {
+                if (nums[midIndexer] == target)
+                {
+                    return new int[] { FindLeftBoundary(nums, target, leftIndexer, midIndexer), FindRightBoundary(nums, target, midIndexer, rightIndexer) };
+                }
+                else if (nums[midIndexer] < target)
+                {
+                    leftIndexer = midIndexer + 1;
+                }
+                else
+                {
+                    rightIndexer = midIndexer - 1;
+                }
+                midIndexer = (leftIndexer + rightIndexer) / 2;
+            }
+            return new int[] { -1, -1 };
+        }
+
+        public int FindLeftBoundary(int[] nums, int target, int leftIndexer, int rightIndexer)
+        {
+            int midIndexer = leftIndexer;
+            while (leftIndexer<=rightIndexer)
+            {
+                midIndexer = (leftIndexer + rightIndexer) / 2;
+                if (nums[midIndexer] == target)
+                {
+                    if (midIndexer==0 || nums[midIndexer] > nums[midIndexer-1])
+                    {
+                        return midIndexer;
+                    }
+                    rightIndexer = midIndexer - 1;
+                } else
+                {
+                    leftIndexer = midIndexer + 1;
+                }
+            }
+            return midIndexer;
+        }
+
+        public int FindRightBoundary(int[] nums, int target, int leftIndexer, int rightIndexer)
+        {
+            int midIndexer = rightIndexer;
+            while (leftIndexer <= rightIndexer)
+            {
+                midIndexer = (leftIndexer + rightIndexer) / 2;
+                if (nums[midIndexer] == target)
+                {
+                    if (midIndexer == nums.Length-1 || nums[midIndexer] < nums[midIndexer + 1])
+                    {
+                        return midIndexer;
+                    }
+                    leftIndexer = midIndexer + 1;
+                }
+                else
+                {
+                    rightIndexer = midIndexer - 1;
+                }
+            }
+            return midIndexer;
+        }
+
+
+        public void ReadInput()
+        {
+            _nums = ProblemIO.ReadIntArrayFromConsole("Enter array of integers (nums):");
+            _target = ProblemIO.ReadIntFromConsole("Enter target element (target):");
+        }
+
+        public void ShowAnswer()
+        {
+            ProblemIO.WriteValue(_output);
+        }
+
+        public void Solve()
+        {
+            _output = SearchRange(_nums, _target);
+        }
+    }
+}
---
+++ CodePracticeLeetCodeTests/Problem0034_FindFirstAndLastPositionOfElementInSortedArray_Tests.cs
@@ -0,0 +1,75 @@
+﻿namespace CodePracticeLeetCodeTests
+{
+    internal class Problem0034_FindFirstAndLastPositionOfElementInSortedArray_Tests
+    {
+        private Problem0034_FindFirstAndLastPositionOfElementInSortedArray problem;
+        [SetUp]
+        public void Setup()
+        {
+            problem = new Problem0034_FindFirstAndLastPositionOfElementInSortedArray();
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            int[] nums = { 5,7,7,8,8,10 };
+            int target = 8;
+            int[] output = {3,4};
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            int[] nums = { 5, 7, 7, 8, 8, 10 };
+            int target = 6;
+            int[] output = { -1, -1 };
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            int[] nums = { };
+            int target = 0;
+            int[] output = { -1, -1};
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            int[] nums = { 1,2 };
+            int target = 1;
+            int[] output = { 0, 0 };
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase5()
+        {
+            int[] nums = {1 };
+            int target = 2;
+            int[] output = { -1, -1 };
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase6()
+        {
+            int[] nums = { 1 };
+            int target = 1;
+            int[] output = { 0, 0 };
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase7()
+        {
+            int[] nums = { 1, 1, 1, 1 };
+            int target = 1;
+            int[] output = { 0, 3 };
+            Assert.That(problem.SearchRange(nums, target), Is.EqualTo(output));
+        }
+    }
+}
