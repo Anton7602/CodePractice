b4aaca4f934c189cd7533669df12e4bab2444f06
Антон <Strateg7602@gmail.com>
2024-04-08 09:02:22 +0300

    LeetCode Problem 35 - SearchInsertPosition Solved

---
+++ CodePracticeLeetCode/Problem0035_SearchInsertPosition.cs
@@ -0,0 +1,50 @@
+﻿namespace CodePracticeLeetCode
+{
+    public class Problem0035_SearchInsertPosition : IProblem
+    {
+        private int[] _nums { get; set; } = new int[0];
+        private int _target { get; set; }
+        private int _output { get; set; }
+
+        public int SearchInsert(int[] nums, int target)
+        {
+            int leftIndexer = 0;
+            int rightIndexer = nums.Length - 1;
+            int midIndexer = (leftIndexer + rightIndexer) / 2;
+            while (leftIndexer <= rightIndexer)
+            {
+                if (nums[midIndexer] < target)
+                {
+                    if (midIndexer == nums.Length - 1) return nums.Length;
+                    leftIndexer = midIndexer + 1;
+                }
+                else
+                {
+                    if (midIndexer == 0) return 0;
+                    rightIndexer = midIndexer - 1;
+                }
+                if (nums[midIndexer] == target || (midIndexer>0 && nums[midIndexer - 1] < target && nums[midIndexer] > target))
+                {
+                    return midIndexer;
+                }
+                midIndexer = (leftIndexer + rightIndexer) / 2;
+            }
+            return midIndexer;
+        }
+        public void ReadInput()
+        {
+            _nums = ProblemIO.ReadIntArrayFromConsole("Enter array of integers (nums):");
+            _target = ProblemIO.ReadIntFromConsole("Enter target element (target):");
+        }
+
+        public void ShowAnswer()
+        {
+            ProblemIO.WriteValue(_output);
+        }
+
+        public void Solve()
+        {
+            _output = SearchInsert(_nums, _target);
+        }
+    }
+}
---
+++ CodePracticeLeetCodeTests/Problem0035_SearchInsertPosition_Tests.cs
@@ -0,0 +1,93 @@
+﻿namespace CodePracticeLeetCodeTests
+{
+    internal class Problem0035_SearchInsertPosition_Tests
+    {
+        private Problem0035_SearchInsertPosition problem;
+        [SetUp]
+        public void Setup()
+        {
+            problem =  new Problem0035_SearchInsertPosition();
+        }
+
+        [Test]
+        public void TestCase1()
+        {
+            int[] nums = { 1,3,5,6 };
+            int target = 5;
+            int output = 2;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase2()
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            int target = 2;
+            int output = 1;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase3()
+        {
+            int[] nums = { 1, 3, 5, 6 };
+            int target = 7;
+            int output = 4;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase4()
+        {
+            int[] nums = { 3 };
+            int target = 4;
+            int output = 1;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase5()
+        {
+            int[] nums = { 3 };
+            int target = 3;
+            int output = 0;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase6()
+        {
+            int[] nums = { 1, 2, 4, 5, 7, 8, 9, };
+            int target = 6;
+            int output = 4;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase7()
+        {
+            int[] nums = { 1, 2, 4, 5, 7, 8, 9 };
+            int target = -1;
+            int output = 0;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase8()
+        {
+            int[] nums = { 1, 2, 4, 5, 7, 8, 9 };
+            int target = 9;
+            int output = 6;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+
+        [Test]
+        public void TestCase9()
+        {
+            int[] nums = { 1, 4 };
+            int target = 2;
+            int output = 1;
+            Assert.That(problem.SearchInsert(nums, target), Is.EqualTo(output));
+        }
+    }
+}
